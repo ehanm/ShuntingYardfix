dbf3506919e3d502fc8ace5aa0f9d3ffbcef8881
U-BSD\452735 <452735@SHS-10D9C064E.beaverton.k12.or.us>
2022-02-10 13:37:37 -0800

    first push

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+
+  char input[1000];
+
+  char input2[1000];
+
+  cout << "Shunting Algorithm!" << endl;
+
+  cout << "Enter a mathematical expression (make sure to include spaces): " << endl;
+
+  cin >> input;
+
+  cout << "Prefix, Infix, or Postfix?" << endl;
+
+  cin >> input2;
+
+  // enter converting to prefix, infix, and postfix here
+
+  
+
+
+
+}
