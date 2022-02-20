c5a44684bb54497b3cde3d2bc2a4f827b4fa2d8c
ehanm <90476888+ehanm@users.noreply.github.com>
2022-02-20 11:21:21 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
+dit Options Buffers Tools C++ Help
 #include <iostream>
 #include <cstring>
 #include "stack.h"
+#include "queue.h"
 // this is shunting yard
 
 using namespace std;
 
-void push(char z, Stack* next);
+void push(char z, Stack* &head);
+void enqueue(char z, Queue* &head);
+
 
 int main(){
 
   Stack* stackhead = NULL;
   Queue* queuehead = NULL;
-  
+
   char input[100];
-  
+
   char input2[100];
 
   cout << "Shunting Algorithm!" << endl;
@@ -33,18 +37,29 @@ int main(){
 
       if (i = 0){
 
-	// enqueue 
-      
+        // enqueue
+
       }
 
-      if (input[i] = '+' || input[i] = '-' || input[i] = '*' || input[i] = '/' || input[i] = '^'){
+      else if (input[i] = '+' || input[i] = '-' || input[i] = '*' || input[i] = '/' || input[i] = '^' || input[i] = '('){
 
-	// push
-	
-      
+        push(input[i], stackhead);
+
       }
-      
+
+      else if (input[i] = '1' || input[i] = '2' || input[i] = '3' || input[i] = '4' || input[i] = '5' ||
+               input[i] = '6' || input[i] = '7' || input[i] = '8' || input[i] = '9'){
+
+        enqueue(input[i], queuehead);
+
+
+      }
+
     }
+
+    // move stuff from stack to queue
+
+    // print queue
   }
 
   else if (strcmp(input2, "Prefix") == 0) {
@@ -57,13 +72,36 @@ int main(){
 
 }
 
-void push(char z, Stack* next){
-  if (next == NULL){
+void push(char z, Stack* &head) {
+  Stack* newpointer = new Stack();
+  newpointer->x = z;
+  newpointer->next = head;
+  head = newpointer;
+}
 
-    Stack* newpointer = new Stack();
-    nextpointer->z = x;
+void enqueue(char z, Queue* &head) {
 
+
+  if (head == NULL){
+    Queue* newpointer = new Queue();
+    newpointer->x = z;
+    head = newpointer;
 
   }
-  
+
+  else if (head->next == NULL) {
+    Queue* newpointer = new Queue();
+    newpointer->x = z;
+    head->next = newpointer;
+
+  }
+
+  else {
+
+    enqueue(z, head->next);
+
+  }
+
 }
+
+
